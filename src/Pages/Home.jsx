c5225f30c4e94b51c8a2e27817a5dc140adb4acb
Yunus <128239091+theyuunus@users.noreply.github.com>
2023-12-25 22:07:28 +0500

    #fix #add about of home page #Home About

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Helmet from '../components/Helmet/Helmet'
 import HomeHeader from '../components/HomeHeader/HomeHeader'
+import HomeAbout from '../components/HomeAbout/HomeAbout'
 
 export default function Home() {
   return (
     <Helmet title="Home">
       <HomeHeader />
+      <HomeAbout />
     </Helmet>
   )
 }
