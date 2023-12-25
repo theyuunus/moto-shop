ce11facb7f003573d14b2f0f5f6667c8822ff05a
Yunus <128239091+theyuunus@users.noreply.github.com>
2023-12-25 12:11:20 +0500

    #fix #add Contact

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Helmet from '../components/Helmet/Helmet'
+import Contact from '../components/Contact/Contact'
 
-export default function Contact() {
+export default function contact() {
   return (
-   <Helmet title="Contact">
-      <div>Contact</div>
-   </Helmet>
+    <Helmet title="Contact">
+      <Contact />
+    </Helmet>
   )
 }
