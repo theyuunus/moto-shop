701c13aa42b9f90535e8f6da0a906f6ce5c576af
Yunus <128239091+theyuunus@users.noreply.github.com>
2023-12-27 23:18:44 +0500

    #fix #fix and upgrade nav, media #Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,23 @@
-import React, { useRef, useState } from 'react'
-import './Navbar.scss'
-import { Link } from 'react-router-dom'
+import React, { useRef, useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import './Navbar.scss';
 
 export default function Navbar() {
     const [isMenuActive, setMenuActive] = useState(false);
     const menuRef = useRef(null);
 
+    const closeMenu = () => {
+        menuRef.current.classList.remove('menu_active');
+        setMenuActive(false);
+    };
+
     const toggleMenu = () => {
-        menuRef.current.classList.toggle("menu_active");
-        setMenuActive(!isMenuActive);
+        if (isMenuActive) {
+            closeMenu();
+        } else {
+            menuRef.current.classList.add('menu_active');
+            setMenuActive(true);
+        }
     };
     return (
         <React.Fragment>
@@ -16,43 +25,45 @@ export default function Navbar() {
                 <div className='container'>
                     <div className='navbar-nav'>
                         <div className="navbar-nav-logo">
-                            <Link to={"/"}>
+                            <Link to={"/"} onClick={closeMenu}>
                                 <h1 className="navbar-nav-logo-h1">
                                     two wheelers
                                 </h1>
                             </Link>
                         </div>
                         <div className='navbar-nav-menu'>
-                            <Link to={"/"} className='navbar-nav-menu-a'>
+                            <NavLink to={"/"}
+                                className='navbar-nav-menu-a' onClick={closeMenu}>
                                 <li>
                                     Home
                                 </li>
-                            </Link>
-                            <Link to={"showroom"} className='navbar-nav-menu-a'>
+                            </NavLink>
+                            <NavLink to={"/showroom"} className='navbar-nav-menu-a' onClick={closeMenu}>
                                 <li>
                                     Motorcycles
                                 </li>
-                            </Link>
-                            <Link to={"blog"} className='navbar-nav-menu-a'>
+                            </NavLink>
+                            <NavLink to={"/blog"} className='navbar-nav-menu-a' onClick={closeMenu}>
                                 <li>
                                     Our Blog
                                 </li>
-                            </Link>
-                            <Link to={"contact"} className='navbar-nav-menu-a'>
+                            </NavLink>
+                            <NavLink to={"/contact"} className='navbar-nav-menu-a' onClick={closeMenu}>
                                 <li>
                                     Contact Us
                                 </li>
-                            </Link>
-                            <Link to={"login"}>
+                            </NavLink>
+                            <NavLink to={"/login"}
+                                onClick={closeMenu}>
                                 <button className='navbar-nav-menu-button'>
                                     Login
                                 </button>
-                            </Link>
+                            </NavLink >
                             <div className='app_menu_icon' onClick={toggleMenu}>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" fill="none">
-                                    <path d="M30 5.5L4.0794e-07 5.5" stroke="black" stroke-width="3" />
-                                    <path d="M30 14.5L4.0794e-07 14.5" stroke="black" stroke-width="3" />
-                                    <path d="M30 24.5L4.0794e-07 24.5" stroke="black" stroke-width="3" />
+                                    <path d="M30 5.5L4.0794e-07 5.5" stroke="black" strokeWidth="3" />
+                                    <path d="M30 14.5L4.0794e-07 14.5" stroke="black" strokeWidth="3" />
+                                    <path d="M30 24.5L4.0794e-07 24.5" stroke="black" strokeWidth="3" />
                                 </svg>
                             </div>
                         </div>
@@ -61,28 +72,28 @@ export default function Navbar() {
             </nav>
             <div className='app_navbar' ref={menuRef}>
                 <div className='app_navbar-menu'>
-                    <Link to={"/"} className='app_navbar-menu-a'>
+                    <NavLink to={"/"} activeClassName="active" className='app_navbar-menu-a' onClick={closeMenu}>
                         <li>
                             Home
                         </li>
-                    </Link>
-                    <Link to={"showroom"} className='app_navbar-menu-a'>
+                    </NavLink>
+                    <NavLink to={"/showroom"} activeClassName="active" className='app_navbar-menu-a' onClick={closeMenu}>
                         <li>
                             Motorcycles
                         </li>
-                    </Link>
-                    <Link to={"blog"} className='app_navbar-menu-a'>
+                    </NavLink>
+                    <NavLink to={"/blog"} activeClassName="active" className='app_navbar-menu-a' onClick={closeMenu}>
                         <li>
                             Our Blog
                         </li>
-                    </Link>
-                    <Link to={"contact"} className='app_navbar-menu-a'>
+                    </NavLink>
+                    <NavLink to={"/contact"} activeClassName="active" className='app_navbar-menu-a' onClick={closeMenu}>
                         <li>
                             Contact Us
                         </li>
-                    </Link>
+                    </NavLink>
                 </div>
             </div>
         </React.Fragment>
-    )
+    );
 }
