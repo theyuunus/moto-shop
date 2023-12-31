5f17d322cfcac3b7d72d0c38e8dece5e3111128d
Yunus <128239091+theyuunus@users.noreply.github.com>
2023-12-31 16:46:47 +0500

    #fix #add Login #Login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Login.scss';
+import axios from 'axios';
 
 export default function Login() {
     const [showFirstInputs, setShowFirstInputs] = useState(true);
@@ -7,12 +8,20 @@ export default function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
+    const [formData, setFormData] = useState({ email: '', password: '' });
+    const [user, setUser] = useState(null);
+    const [isLoggedIn, setIsLoggedIn] = useState(false); 
 
     const toggleInputs = () => {
         setShowFirstInputs((prev) => !prev);
     };
 
-    const submitForm = () => {
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prev) => ({ ...prev, [name]: value }));
+    };
+
+    const submitForm = async () => {
         if (password !== confirmPassword) {
             alert("Password and Confirm Password must match.");
             return;
@@ -25,20 +34,52 @@ export default function Login() {
             confirmPassword: confirmPassword
         };
 
-        fetch('http://localhost:6060/user', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(jsonData),
-        })
-            .then(response => response.json())
-            .then(data => {
-                console.log('Response from the server:', data);
-            })
-            .catch(error => {
-                console.error('Error:', error);
+        try {
+            const response = await fetch('http://localhost:6060/user', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(jsonData),
             });
+
+            const data = await response.json();
+            alert('Registration success: ' + data.name); // Corrected alert message
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.get('http://localhost:6060/user');
+            const data = response.data;
+            const foundUser = data.find((item) => item.email === formData.email && item.password === formData.password);
+
+            if (foundUser) {
+                performLogin(foundUser);
+            } else {
+                alert('Login error: User not found');
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    useEffect(() => {
+        const storedUser = JSON.parse(localStorage.getItem('User'));
+        if (storedUser) {
+            setUser(storedUser);
+            setIsLoggedIn(true);
+        }
+    }, []);
+
+    const performLogin = (user) => {
+        alert('Login successful: ' + user.name);
+        setUser(user);
+        setIsLoggedIn(true);
+        localStorage.setItem('User', JSON.stringify(user));
     };
 
     return (
@@ -69,23 +110,30 @@ export default function Login() {
                     </h3>
                     <div className='login-left-inputs'>
                         {showFirstInputs ? (
-                            <>
+                            <form className='login-left-inputs-form' onSubmit={handleSubmit}>
                                 <input
-                                    type='email'
-                                    placeholder='Email'
-                                    className='login-left-inputs-input'
+                                    type="email"
+                                    placeholder="Email"
+                                    name="email"
+                                    value={formData.email}
+                                    onChange={handleChange}
+                                    className="login-left-inputs-input"
                                     required
                                 />
                                 <input
-                                    type='password'
-                                    placeholder='Password'
-                                    className='login-left-inputs-input'
+                                    type="password"
+                                    placeholder="Password"
+                                    name="password"
+                                    value={formData.password}
+                                    onChange={handleChange}
+                                    className="login-left-inputs-input"
                                     required
                                 />
-                                <button type='button' className='login-left-inputs-button'>
+                                <button type="submit" className="login-left-inputs-button">
                                     Login
                                 </button>
-                            </>
+                            </form>
+
                         ) : (
                             <>
                                 <input
