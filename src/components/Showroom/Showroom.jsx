6c320e7ce4b03a9bb3785c9cbd1258b390043b9d
Yunus <128239091+theyuunus@users.noreply.github.com>
2024-01-27 20:10:31 +0500

    #fix #js #scss add background img of showroom

---
+++ src/components/Showroom/Showroom.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Showroom.scss'
+
+export default function Showroom() {
+  return (
+    <React.Fragment>
+      <div className="showroom">
+
+      </div>
+    </React.Fragment>
+  )
+}
