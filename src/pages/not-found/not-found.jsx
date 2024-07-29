9e2864dfc52f31a15278321ae7a680dd04c6b6e2
Yunus <theyuunus@gmail.com>
2024-07-29 20:10:47 +0500

    #fix #jsx 404 page was finished

--- src/pages/not-found/not-found.jsx
+++ src/pages/not-found/not-found.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
 import './not-found.scss'
+import Title from '../../components/texts/title'
+import Text from '../../components/texts/text'
+import { Link } from 'react-router-dom'
+import notFound from '../../images/not-found.png'
 
 const NotFound = () => {
     return (
-        <>
-            404
-        </>
+        <div className='notFound'>
+            <div className='notFound-content'>
+                <div className='notFound-titles'>
+                    <Title
+                        color='rgba(37, 44, 51, 1)' className='notFound-title'
+                    >Oops....</Title>
+                    <Title
+                        color='rgba(37, 44, 51, 1)'
+                        as='h2'
+                        className='notFound-subtitle'
+                    >Page not found</Title>
+                </div>
+
+                <Text
+                    color='rgba(37, 44, 51, 1)'
+                    className='notFound-text'
+                >Unfortunately, the page you’re looking for is either under maintenance, unaccessible, or there’s a server problem in our site. Please go back to home page.
+                </Text>
+
+                <Link to="/" className='notFound-link'>
+                    Go back to Home
+                </Link>
+            </div>
+
+            <div className='notFound-image-container'>
+                <img src={notFound} alt="Not Found" className='notFound-image' />
+            </div>
+        </div>
     )
 }
 
